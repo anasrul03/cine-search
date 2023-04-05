853f3687530a0bcc83073bd9a5062d5b58659734
Amarnath911 <drctramar@gmail.com>
2023-04-05 17:37:19 +0800

    MovideDetails Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import MovieCard from "./Components/MovieCard";
+import MovieDetails from "./Components/MovieDetails";
 
 function App() {
   return (
     <div>
       <MovieCard />
+      <MovieDetails />
+      
     </div>
   );
 }
