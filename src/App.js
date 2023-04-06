1ad4fed27dbd5b307b72fb65250fd9adbc319530
Amarnath911 <62863757+Amarnath911@users.noreply.github.com>
2023-04-06 17:17:51 +0800

    MovideDetails Created (#6)
    * MovideDetails Created
    
    * Movie Details and App.JS updated
    
    * Updated Scss, Movie Details JSX and AppJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import MovieSearch from "./Components/MovieSearch";
+import MovieDetails from "./Components/MovieDetails";
 import MovieCard from "./Components/MovieCard";
+import './Components/Styles/MovieDetails.scss';
 
 function App() {
+
   return (
-    <div>
-      <MovieCard />
-    </div>
+    <BrowserRouter>
+ 
+      <div>
+        <MovieCard/>
+
+      </div>
+      <Routes>
+      <Route path="/" element={<MovieSearch />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
