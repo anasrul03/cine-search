5294e19252725a83c245850246d3981b8aad2108
Amarnath911 <62863757+Amarnath911@users.noreply.github.com>
2023-04-08 11:52:09 +0800

    Moviedetails (#12)
    * MovideDetails Created
    
    * Movie Details and App.JS updated
    
    * Updated Scss, Movie Details JSX and AppJS
    
    * New Component has been created for Latest & Popular Movies, App.JS, Movie Card and Movie Details has been updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 // import MovieSearch from "./Components/MovieSearch";
 import MovieDetails from "./Components/MovieDetails";
 import "./Components/Styles/MovieDetails.scss";
-import { LatestMovie, PopularMovie } from "./Components/Section";
+import MainpageList from "./Components/MainpageList";
+
 
 function App() {
   return (
+    
     <BrowserRouter>
-      <div>
-        <h1>Now Showing</h1>
-        <LatestMovie />
-        <h1>Trending Now</h1>
-        <PopularMovie />
-      </div>
       <Routes>
         {/* <Route path="/" element={<MovieSearch />} /> */}
-        {/* <Route path="/movie/:id" element={<MovieDetails />} /> */}
+        <Route path="/" element={<MainpageList />}/>
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
   );
